972ea7969bb3131b0ca3c890829a56833078a648
Mucahityigit <bmy_746@hotmail.com>
2024-02-02 11:55:16 +0300

    hata durumu kontrol edildi

--- src/container/RecommendedTrends.jsx
+++ src/container/RecommendedTrends.jsx
@@ -3,29 +3,40 @@ import { useDispatch, useSelector } from "react-redux";
 import { getAllTimeTopTracks, getMonthlyTopTracks } from "../redux/trackSlice";
 import Loading from "../components/Loading";
 import TrendCards from "../components/TrendCards";
+
 const RecommendedTrends = () => {
   const dispatch = useDispatch();
   const { allTimeTopTracks, monthlyTopTracks } = useSelector(
     (state) => state.track
   );
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    dispatch(getAllTimeTopTracks());
-    dispatch(getMonthlyTopTracks());
+    const fetchData = async () => {
+      try {
+        await dispatch(getAllTimeTopTracks());
+        await dispatch(getMonthlyTopTracks());
+      } catch (error) {
+        if (error.response && error.response.status === 429) {
+          // Handle 429 error
+          setError("API limit exceeded. Please try again later.");
+        } else {
+          // Handle other errors
+          setError("An error occurred while fetching data.");
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
   }, [dispatch]);
-
-  useEffect(() => {
-    if (
-      Object.keys(allTimeTopTracks).length > 0 &&
-      Object.keys(monthlyTopTracks).length > 0
-    ) {
-      setIsLoading(false);
-    }
-  }, [allTimeTopTracks, monthlyTopTracks]);
 
   if (isLoading) {
     return <Loading />;
+  } else if (error) {
+    return <div className="flex w-full flex-col gap-5">{error}</div>;
   } else {
     return (
       <div className="flex w-full flex-col gap-5">
